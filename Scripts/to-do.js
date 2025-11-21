2c12b8d86c35758fbfce47576bc95334404402b0
MangoSealer <danilocello@hotmail.com>
2025-11-21 20:18:39 -0300

    1.7  -  teste_1 do site fake

---
+++ Scripts/to-do.js
@@ -0,0 +1,14 @@
+const tarefa = document.querySelector("#tarefa");
+const btn = document.querySelector("#btn");
+const lista = document.querySelector("#lista");
+
+
+
+btn.addEventListener("click", function () {
+    // if (tarefa.value == "") {
+    //     alert("bota algo")
+    // }
+    alert("já existe um usuário com essa senha")
+}
+)
+
---
+++ service-worker.js
@@ -0,0 +1,54 @@
+const CACHE_NAME = 'to-do-v1';
+const urlsToCache = [
+  '/paginas/to-do.html', 
+  '/index.html',
+  '/styles/main.css',
+  '/scripts/app.js',
+  '/img/icon-512x512.png'
+];
+
+
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        // Armazena todos os arquivos definidos na lista
+        return cache.addAll(urlsToCache); 
+      })
+  );
+});
+
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request) // Tenta encontrar no cache
+      .then(response => {
+        // Se encontrar no cache, retorna o recurso salvo
+        if (response) { 
+          return response;
+        }
+        
+        // Caso contrário, vai para a rede para buscar o recurso
+        return fetch(event.request); 
+      })
+  );
+});
+
+
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            // Exclui caches não atuais
+            return caches.delete(cacheName); 
+          }
+        })
+      );
+    })
+  );
+});
