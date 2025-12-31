fc399ba74166f9222f6a48696c20dda3fc30492a
MangoSealer <danilocello@hotmail.com>
2025-12-31 13:48:23 -0300

    a

--- Hub/testes/to-do.js
+++ Hub/testes/to-do.js
@@ -1,7 +1,95 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getFirestore, collection, addDoc, onSnapshot, deleteDoc, doc }
-    from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+// import { getFirestore, collection, addDoc, onSnapshot, deleteDoc, doc }
+//     from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
+
+// const firebaseConfig = {
+//     apiKey: "AIzaSyC2l6Rp1_L_udZyBuYWVuhhd9lSyRH-qPM",
+//     authDomain: "to-do-397d8.firebaseapp.com",
+//     projectId: "to-do-397d8",
+//     storageBucket: "to-do-397d8.firebasestorage.app",
+//     messagingSenderId: "791824707186",
+//     appId: "1:791824707186:web:9b2e663117be126b869ceb"
+// };
+
+
+// const app = initializeApp(firebaseConfig);
+// const db = getFirestore(app);
+// const dbCollection = collection(db, "testes");
+
+
+// const tarefa = document.querySelector("#tarefa");
+// const btn = document.querySelector("#btn");
+// const lista = document.querySelector("#lista");
+
+
+// tarefa.addEventListener("keypress", function (event) {
+
+//     if (event.key === "Enter") {
+//         event.preventDefault();
+//         btn.click();
+//     }
+// });
+
+
+// btn.addEventListener("click", async function (event) {
+//     event.preventDefault();
+//     if (tarefa.value !== "") {
+//         try {
+//             await addDoc(dbCollection, { nome: tarefa.value });
+//             tarefa.value = "";
+//         } catch (error) {
+//             console.error("Erro ao adicionar: ", error);
+//         }
+//     } else {
+//         alert("Escreva algo!");
+//     }
+// });
+
+
+// onSnapshot(dbCollection, (snapshot) => {
+//     lista.innerHTML = "";
+
+//     snapshot.forEach((item) => {
+//         const dados = item.data();
+//         const id = item.id;
+
+//         lista.innerHTML += `
+//             <li data-id="${id}">
+//                 <i class="fa-solid fa-genderless fa-sm "></i>
+//                 <span> ${dados.nome}</span>
+//                 <i class="fa-solid fa-ban fa-xs close" style="cursor: pointer;"></i>
+//             </li>
+//         `;
+//     });
+// });
+
+// // remover
+// lista.addEventListener("click", async function (event) {
+//     const botaoClicado = event.target.closest(".close");
+//     if (botaoClicado) {
+//         const itemLi = botaoClicado.parentElement;
+//         const idParaRemover = itemLi.getAttribute("data-id");
+//         if (idParaRemover) {
+//             await deleteDoc(doc(db, "testes", idParaRemover));
+//         }
+//     }
+
+
+// });
+
+
+
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import {
+    getFirestore,
+    collection,
+    addDoc,
+    onSnapshot,
+    deleteDoc,
+    doc
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
     apiKey: "AIzaSyC2l6Rp1_L_udZyBuYWVuhhd9lSyRH-qPM",
@@ -12,41 +100,41 @@ const firebaseConfig = {
     appId: "1:791824707186:web:9b2e663117be126b869ceb"
 };
 
-
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const dbCollection = collection(db, "testes");
-
 
 const tarefa = document.querySelector("#tarefa");
 const btn = document.querySelector("#btn");
 const lista = document.querySelector("#lista");
 
-
-tarefa.addEventListener("keypress", function (event) {
-
+/* Enter adiciona */
+tarefa.addEventListener("keypress", (event) => {
     if (event.key === "Enter") {
         event.preventDefault();
         btn.click();
     }
 });
 
-
-btn.addEventListener("click", async function (event) {
+/* Adicionar tarefa */
+btn.addEventListener("click", async (event) => {
     event.preventDefault();
+
     if (tarefa.value !== "") {
         try {
-            await addDoc(dbCollection, { nome: tarefa.value });
+            await addDoc(dbCollection, {
+                nome: tarefa.value
+            });
             tarefa.value = "";
         } catch (error) {
-            console.error("Erro ao adicionar: ", error);
+            console.error("Erro ao adicionar:", error);
         }
     } else {
         alert("Escreva algo!");
     }
 });
 
-
+/* Renderização */
 onSnapshot(dbCollection, (snapshot) => {
     lista.innerHTML = "";
 
@@ -55,25 +143,29 @@ onSnapshot(dbCollection, (snapshot) => {
         const id = item.id;
 
         lista.innerHTML += `
-            <li data-id="${id}">
-                <i class="fa-solid fa-genderless fa-sm "></i>
-                <span> ${dados.nome}</span>
-                <i class="fa-solid fa-ban fa-xs close" style="cursor: pointer;"></i>
-            </li>
+            <div class="item my-form" id="checklist" data-id="${id}">
+                <input type="checkbox" class="check">
+                <label>${dados.nome}</label>
+                <i class="fa-solid fa-ban fa-xs close" style="cursor:pointer;"></i>
+            </div>
         `;
     });
 });
 
-// remover
-lista.addEventListener("click", async function (event) {
+/* Remover */
+lista.addEventListener("click", async (event) => {
     const botaoClicado = event.target.closest(".close");
+
     if (botaoClicado) {
-        const itemLi = botaoClicado.parentElement;
-        const idParaRemover = itemLi.getAttribute("data-id");
+        const item = botaoClicado.closest(".item");
+        const idParaRemover = item.getAttribute("data-id");
+
         if (idParaRemover) {
             await deleteDoc(doc(db, "testes", idParaRemover));
         }
     }
-
-
 });
+
+
+
+
