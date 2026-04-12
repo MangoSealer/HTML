335c8fdc556d5347ecef3fe10ec416f69272afb2
MangoSealer <danilocello@hotmail.com>
2026-04-12 19:41:10 -0300

    4.4  -  adicionei checklist dos reminders

---
+++ hub/reminders/to-do.js
@@ -0,0 +1,88 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getFirestore, collection, addDoc, onSnapshot, deleteDoc, doc, query, orderBy }
+    from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+
+// configurações do app
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC2l6Rp1_L_udZyBuYWVuhhd9lSyRH-qPM",
+    authDomain: "to-do-397d8.firebaseapp.com",
+    projectId: "to-do-397d8",
+    storageBucket: "to-do-397d8.firebasestorage.app",
+    messagingSenderId: "791824707186",
+    appId: "1:791824707186:web:9b2e663117be126b869ceb"
+};
+
+// inicializar o aplicativo
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const dbCollection = collection(db, "reminders");
+
+
+const q = query(dbCollection, orderBy("criadoEm"));
+const tarefa = document.querySelector("#tarefa");
+const btn = document.querySelector("#btn");
+const lista = document.querySelector("#lista");
+
+
+// Enter
+tarefa.addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        btn.click();
+    }
+});
+
+// Adicionador
+btn.addEventListener("click", async function (event) {
+    event.preventDefault();
+
+    if (tarefa.value !== "") {
+        const valor = tarefa.value;
+        tarefa.value = "";
+
+        try {
+
+            await addDoc(dbCollection, {
+                nome: valor,
+                criadoEm: Date.now()
+            });
+        } catch (error) {
+            console.error("Erro ao adicionar: ", error);
+        }
+    } else {
+        alert("Escreva algo!");
+    }
+});
+
+// template
+
+onSnapshot(q, (snapshot) => {
+    lista.innerHTML = "";
+
+    snapshot.forEach((item) => {
+        const dados = item.data();
+        const id = item.id;
+
+        lista.innerHTML += `
+            <li class="item my-form" data-id="${id}">
+                <i class="fa-solid fa-genderless fa-sm "></i>
+                <span> ${dados.nome}</span>
+                <i class="fa-solid fa-ban fa-xs close" style="cursor: pointer;"></i>
+            </li>
+        `;
+    });
+});
+
+// remover
+lista.addEventListener("click", async function (event) {
+    const botaoClicado = event.target.closest(".close");
+    if (botaoClicado) {
+        const itemLi = botaoClicado.parentElement;
+        const idParaRemover = itemLi.getAttribute("data-id");
+        if (idParaRemover) {
+            await deleteDoc(doc(db, "reminders", idParaRemover));
+        }
+    }
+});
